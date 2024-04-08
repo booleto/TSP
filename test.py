0472bcc631da934fe3dcc81ce7026cfd6eddbf92
booleto <exaccxd@gmail.com>
2024-04-08 19:51:10 +0700

    test dynamic programming

--- test.py
+++ test.py
@@ -6,7 +6,6 @@ np.random.seed(42)  # Đảm bảo kết quả nhất quán
 
 def createNodes(N, scale=100):
     """Tạo nút ngẫu nhiên"""
-    # Tạo một danh sách các thành phố (nút) với tọa độ ngẫu nhiên
     return np.random.rand(N, 2) * scale
 
 class AroraQuadTree:
@@ -31,7 +30,7 @@ class AroraQuadTree:
         Cần phải nhập nodes và bounds trước khi gọi hàm này
         """
         assert self.bound != 0, "Cần nhập bound của cây trước!"
-        assert self.bound > np.max(nodes), "Bounds không đủ to để chứa mọi node!"
+        assert self.bound > np.max(self.nodes), "Bounds không đủ to để chứa mọi node!"
         assert len(self.nodes), "Cần nhập danh sách node cho cây!"
         self.root.vertex = self.nodes
         self.root.bbox.bound = np.array((self.bound, self.bound))
@@ -62,14 +61,15 @@ class TreeNode:
     def __init__(self, box=(0,0,0,0)) -> None:
         self.level = 0
         self.parent = None
+        self.children = None
+        self.neighbors = []
         self.bbox = BBox(box)
         self.is_leaf = False
-        self.portals = []
-        self.children = None
+        self.portals = np.array([]).reshape((0, 2))
         self.local_idx = -1
         self.vertex = None
         self.directions = []
-        self.solutions = []
+        self.solutions = {}
 
 
     def divide(node):
@@ -88,6 +88,9 @@ class TreeNode:
         node.children[2].bbox.pos = node.bbox.pos + np.array((b, 0))
         node.children[3].bbox.pos = node.bbox.pos + np.array((b, b))
 
+        for i in range(4):
+            node.children[i].local_idx = i
+
         for child in node.children:
             child.vertex = node.vertex[[child.bbox.is_inside(m) for m in node.vertex]]
             if len(child.vertex) == 0:
@@ -97,51 +100,43 @@ class TreeNode:
                 continue
             TreeNode.divide(child)
 
-        # for i in range(4): # các hướng cần thêm portal
-        #     node.children[i].local_idx = i
-        #     if len(node.children[i].vertex) > 0:
-        #         for j in [idx for idx in range(4) if idx != i]:
-        #             if len(node.children[j].vertex) != 0:
-        #                 node.children[j].directions.append(TreeNode.idx_to_vec(j, i))
+        for i1 in range(4): # Thêm portals
+            for i2 in range(i1+1, 4):
+                if len(node.children[i1].vertex) > 0 and len(node.children[i2].vertex) > 0:
+                    dir = TreeNode.idx_to_vec(i1, i2)
+                    node.children[i1].directions.append(dir)
+                    node.children[i2].directions.append(- dir)
+                    portals = TreeNode.calc_portals(node.children[i1].bbox.pos, node.children[i1].bbox.bound[0], dir, m=2)
+                    # print(f"portals: {portals}")
+                    # print(f"node portals: {node.children[i].portals}")
 
-        for i in range(4):
-            for j in range(i+1, 4):
-                if len(node.children[i].vertex) > 0 and len(node.children[j].vertex) > 0:
-                    node.children[i].directions.append(TreeNode.idx_to_vec(i, j))
-                    node.children[j].directions.append(TreeNode.idx_to_vec(j, i))
-                
-    
-
-    # def solve_leaf(self, r=2):
-    #     portals = []
-    #     for d1 in len(self.directions) - 1:
-    #         for d2 in range(d1+1, len(self.directions)):
-    #             dir = TreeNode.idx_to_vec(self.directions[d1], self.directions[d2])
-    #             portals.append(TreeNode.calc_portals(self.pos, self.bbox.bound[0], dir, m=2))
+                    node.children[i1].portals = np.concatenate((node.children[i1].portals, portals), axis=0)
+                    node.children[i2].portals = np.concatenate((node.children[i2].portals, portals), axis=0)
+                    node.children[i1].neighbors.append(node.children[i2])
+                    node.children[i2].neighbors.append(node.children[i1])
+            
         
-                
 
-    
     def calc_portals(pos: np.array, bound: int, dir: np.array, m: int=1):
         '''Tính toán vị trí các portal
         '''
         bound_vec = np.array((bound, bound))
-        corners = np.array(((-1, -1), (1, 1), (-1, 1), (1, -1)))
+        corners = np.array(((-1, -1), (1, 1), (-1, 1), (1, -1))) # trong góc
         if np.any([np.all(np.equal(corner, dir)) for corner in corners]):
-            bound_vec = (bound_vec + 1) / 2
-            return [pos + bound_vec]
+            dir = (dir + 1) / 2
+            return [pos + np.multiply(bound_vec, dir)]
         
         m += 1
-        if np.all(np.equal(dir, np.array((0, 1)))): # bên phải
+        if np.array_equiv(dir, np.array((0, 1))): # bên phải
             return [pos + np.array((a*(bound / m), bound)) for a in range(1, m)]
         
-        if np.all(np.equal(dir, np.array((1, 0)))): # bên trên            
+        if np.array_equiv(dir, np.array((1, 0))): # bên trên            
             return [pos + np.array((bound, a*(bound/m))) for a in range(1, m)]
         
-        if np.all(np.equal(dir, np.array((0, -1)))): # bên trái
+        if np.array_equiv(dir, np.array((0, -1))): # bên trái
             return [pos + np.array((a*(bound / m), 0)) for a in range(1, m)]
         
-        if np.all(np.equal(dir, np.array((-1, 0)))): # bên dưới
+        if np.array_equiv(dir, np.array((-1, 0))): # bên dưới
             return [pos + np.array((0, a*(bound / m))) for a in range(1, m)]
 
 
@@ -150,24 +145,21 @@ class TreeNode:
         i1v = np.array((i1 // 2, i1 % 2))
         i2v = np.array(((i2 // 2, i2 % 2)))
         return i2v - i1v
+    
+
+    # def solve_subproblem(self, ins, out):
+
 
 
     def draw(self, ax):
         """Vẽ bbox và vertex. Làm tương tự với các nút con
         ax: Matplotlib axes
         """
-        self.bbox.draw(ax)
-        center = self.bbox.center()
-        bound = self.bbox.bound[0]
-        for dir in self.directions:
-            # pos = center + dir * (bound / 2)
-            pointlist = np.array(TreeNode.calc_portals(self.bbox.pos, bound, dir, m=2))
-            print(pointlist)
-            # plt.scatter(pos[0], pos[1], marker='x', c='r')
-            plt.scatter(pointlist[:, 0], pointlist[:, 1], marker='x', c='r')
-            # ax.annotate(self.bbox.pos, (pos[0], pos[1]))
+        self.bbox.draw(ax) # vẽ bbox
 
-        if self.is_leaf:
+        plt.scatter(self.portals[:, 0], self.portals[:, 1], marker='x', c='r') # vẽ portals 
+
+        if self.is_leaf: # vẽ các điểm
             for i in range(len(self.vertex)):
                 plt.scatter(self.vertex[i, 0], self.vertex[i, 1], color='b')
                 ax.annotate(self.level, (self.vertex[i, 0], self.vertex[i, 1]))
@@ -189,6 +181,7 @@ class BBox:
         self.pos = np.array((box[0], box[1]))
         self.bound = np.array((box[2], box[3]))
 
+
     def is_inside(self, p) -> bool:
         """Kiểm tra điểm p có nằm trong hcn không
         Nếu p ở trên cạnh trên hoặc cạnh trái => False
@@ -198,9 +191,30 @@ class BBox:
         """
         return np.all(self.pos < p) and np.all(p <= self.pos + self.bound)
     
+
+    def is_inside_inclusive(self, p) -> bool:
+        """Kiểm tra điểm p có nằm trong hcn không
+        Nếu p ở trên cạnh hcn => True
+
+        p: Tọa độ điểm cần kiểm tra
+        """
+        return np.all(self.pos <= p) and np.all(p <= self.pos + self.bound)
+    
+
+    def is_inside_exclusive(self, p) -> bool:
+        """Kiểm tra điểm p có nằm trong hcn không
+        Nếu p ở trên cạnh hcn => False
+
+        p: Tọa độ điểm cần kiểm tra
+        """
+        return np.all(self.pos <= p) and np.all(p <= self.pos + self.bound)
+    
+
     def center(self) -> np.ndarray:
+        """Tâm của hình chữ nhật"""
         return (self.pos + self.bound / 2)
     
+
     def draw(self, ax):
         """Vẽ HCN
         ax: Matplotlib axes
@@ -209,6 +223,12 @@ class BBox:
         rect = plt.Rectangle(self.pos, width, height, edgecolor='k', facecolor='none', linewidth=0.7, linestyle="-")
         ax.add_patch(rect)
 
+
+# class PortalSet:
+#     def __init__(self) -> None:
+#         self.portals = []
+
+#     def g
 
 
 def next_2power(num):
@@ -233,33 +253,151 @@ def pertub(nodes, epsilon, d):
 
 
 # quadtree demo
-epsilon = 0.5
-L_0 = 16
-d = L_0
-N = 20
-r = 2
+# epsilon = 1.3
+# L_0 = 1000
+# d = L_0
+# N = 60
+# r = 2
 
-nodes = createNodes(N=N, scale=L_0)
-pnodes = pertub(nodes, epsilon, L_0 * np.sqrt(2))
-plt.scatter(pnodes[:, 0], pnodes[:, 1])
-L = np.max(pnodes)
+# nodes = createNodes(N=N, scale=L_0)
+# pnodes = pertub(nodes, epsilon, L_0 * np.sqrt(2))
+# plt.scatter(pnodes[:, 0], pnodes[:, 1])
+# L = np.max(pnodes)
 
-print("Node count post pertub: ", len(pnodes))
+# print("Lượng node sau khi làm tròn: ", len(pnodes))
 
-qtree = AroraQuadTree()
-qtree.nodes = pnodes
-qtree.bound = next_2power(np.max(pnodes))
-qtree.build_tree()
-qtree.draw(figsize=(8, 8))
-
-
+# qtree = AroraQuadTree()
+# qtree.nodes = pnodes
+# qtree.bound = next_2power(np.max(pnodes))
+# qtree.build_tree()
+# qtree.draw(figsize=(7, 7))
 
 
-# i1 = 1
-# i2 = 2
-# i1v = np.array((i1 // 2, i1 % 2))
-# i2v = np.array(((i2 // 2, i2 % 2)))
-# print(i1v - i2v)
 
-# print(TreeNode.idx_to_vec(2, 1))
 
+ins = np.array((0, 3))
+outs = np.array((4, 1))
+
+tnode = TreeNode(box=(0, 0, 4, 4))
+nodes = np.array(((1, 3), (3, 3), (3, 1)))
+tnode.vertex = nodes
+tnode.divide()
+
+
+
+def solve_subproblem(tnode: TreeNode, ins, out):
+    paths = [[ins]]
+    solution = None
+
+    starts = []
+    ends = []
+    travel_order = []
+    min_path = 0
+    for child in tnode.children:
+        if len(child.vertex) == 0:
+            continue
+        if child.bbox.is_inside_inclusive(ins):
+            starts.append(child)
+        if child.bbox.is_inside_inclusive(out):
+            ends.append(child)
+        min_path += 1
+    
+    queue = [[st] for st in starts]
+
+    while queue:
+        current_path = queue.pop()
+        next_poss = current_path[-1].neighbors
+        for nextp in next_poss:
+            if nextp in ends and len(current_path) == min_path-1 and nextp not in current_path:
+                current_path.append(nextp)
+                travel_order.append(current_path)
+            if nextp not in current_path:
+                queue.append(current_path + [nextp])
+
+    return travel_order
+    
+    # for order in travel_order:
+    #     viable_paths = [np.array(ins)]
+    #     for i in range(len(order) - 1):
+    #         dir = TreeNode.idx_to_vec(order[i].local_idx, order[i+1].local_idx)
+    #         ports = TreeNode.calc_portals(order[i].bbox.pos, order[i].bbox.bound[0], dir, m=1)
+    #         print(viable_paths)
+    #         print(ports)
+    #         viable_paths = add_to_path(viable_paths, ports)
+    #     print(viable_paths)
+
+
+def solve_paths(path, ins, out):
+    viable_paths = np.array(ins)
+    for i in range(len(path) - 1):
+        dir = TreeNode.idx_to_vec(path[i].local_idx, path[i+1].local_idx)
+        ports = TreeNode.calc_portals(path[i].bbox.pos, path[i].bbox.bound[0], dir, m=1)
+        viable_paths = add_to_path(viable_paths, np.array(ports))
+    viable_paths = add_to_path(viable_paths, np.array(out))
+
+        
+    #     viable_paths = add_to_path(viable_paths, np.array(ports))
+
+    # viable_paths = add_to_path(viable_paths, out)
+    return viable_paths
+
+
+path = np.array([[(0, 1), (0, 2)], [(1, 1), (1, 2)]])
+next = np.array(((2, 3), (2, 4)))
+
+temppath = np.empty(0)
+npath = []
+
+def add_to_path(pathlist : np.ndarray, next : np.ndarray):
+    npath = []
+
+    if len(pathlist.shape) == 1:
+        if len(next.shape) == 1:
+            npath.append(pathlist, next.reshape(1, 2), axis=0)
+            return npath
+
+        for n in next[:, :]:
+            npath.append
+        
+    if len(next.shape) == 1:
+        for p in pathlist[:, :]:
+            npath.append(np.concatenate((p, next.reshape(1, 2)), axis=0))
+        return npath
+
+    for p in pathlist[:, :]:
+        for n in next[:, :]:
+            npath.append(np.concatenate((p, n.reshape(1, 2)), axis=0))
+    return npath
+
+# def add_to_path(pathlist : np.ndarray, next : np.ndarray):
+#     npath = []
+#     for p in pathlist:
+#         for n in next:
+#             npath.append(np.concatenate((p, n), axis=0))
+#     return np.array(npath)
+
+
+# path = add_to_path(path, next)
+# print(path)
+
+travel_order = solve_subproblem(tnode, ins, outs)
+
+print(travel_order[0])
+
+ret = solve_paths(travel_order[0], ins, outs)
+print(ret)
+
+
+# i am slowly losing it
+
+# torder = solve_subproblem(tnode=tnode, ins=ins, out=outs)
+
+# for path in torder:
+#     print(" - ")
+#     for node in path:
+#         print(node.vertex, end=" ")
+#     print()
+
+# port1 = np.array(((0, 1), (1, 2)))
+# port2 = np.array(((0, 1), (2, 2)))
+# print(np.intersect1d(port1, port2)) 
